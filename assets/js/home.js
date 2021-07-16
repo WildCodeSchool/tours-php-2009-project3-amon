eb79ef25146efef44748707751edddeedf4ca9b8
rickifox <ricardo.pereira.developpeur@gmail.com>
2021-07-16 16:37:55 +0200

    R11 - JS: Solve toggling issues on home pages navbar

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,10 +2,37 @@ import '../styles/home.css';
 
 const slideSource = document.getElementById('header');
 
-document.getElementById('button-down').onclick = function show() {
+function show() {
     slideSource.classList.toggle('fade');
-};
+}
 
-document.getElementById('button-up').onclick = function hide() {
+function hide() {
     slideSource.classList.toggle('fade');
-};
+}
+
+// user hits button down and the navbar is hidden, we show it.
+const btnDown = document.getElementById('button-down');
+
+btnDown.addEventListener('click', (e) => {
+    if ((e.target.id === 'button-down' || (e.target.classList.contains('arrow') && e.target.parentElement.id === 'button-down')) && !slideSource.classList.contains('fade')) {
+        show();
+    }
+}, false);
+
+// user hits button up and the navbar is visible, we hide it.
+const btnUp = document.getElementById('button-up');
+
+btnUp.addEventListener('click', (e) => {
+    if ((e.target.id === 'button-up' || (e.target.classList.contains('arrow') && e.target.parentElement.id === 'button-up')) && slideSource.classList.contains('fade')) {
+        hide();
+    }
+}, false);
+
+// handle navbar visibility status according to user scroll position
+document.addEventListener('scroll', () => {
+    if (window.scrollY < document.body.offsetHeight * 0.35 && slideSource.classList.contains('fade')) {
+        hide();
+    } else if (window.scrollY > document.body.offsetHeight * 0.35 && !slideSource.classList.contains('fade')) {
+        show();
+    }
+}, false);
